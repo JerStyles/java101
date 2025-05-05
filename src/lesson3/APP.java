9882418cdc74f734bb05bb5b5526c181ba5e85fd
Jer <contactjerjer@gmail.com>
2025-05-05 09:24:50 +0800

    add auto clean java task

---
+++ src/lesson3/APP.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+import lesson3.*;
+
+public class APP {
+
+    public static void main (String[] arg) {
+
+        lesson3.PrintMessage output = new lesson3.PrintMessage();
+
+        output.print("OMGGGGGGGG");
+        output.print(545451515);
+    }
+    
+}
+ 
