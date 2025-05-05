f9c4ec0e302391362567e441004075989bec78e4
Jer <contactjerjer@gmail.com>
2025-05-05 10:00:17 +0800

    rm failing auto tasks

---
+++ src/lesson3/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    private int maxSpeed;
+
+    public Car(int speed) {
+        this.setMaxSpeed(speed); 
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
